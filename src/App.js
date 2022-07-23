b8c2c04d48c0b3779f29d917bfa0a03c233ac2ad
DEMETRIUS CARNEIRO MONTEIRO FERREIRA <68928535+demithehomie@users.noreply.github.com>
2022-07-23 02:10:50 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,43 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <text>DMNS</text>
+        <text>DMNS</text>
+        <text>DMNS</text>
+        <text>DMNS</text>
+        <text>DMNS</text>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav class="navbar">
+
+<div class="logo">MUO</div>
+
+<ul class="nav-links">
+
+  <input type="checkbox" id="checkbox_toggle" />
+  <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+
+  <div class="menu">
+    <li><a href="/">Home</a></li>
+    <li><a href="/">About</a></li>
+    <li class="services">
+      <a href="/">Services</a>
+      
+      <ul class="dropdown">
+        <li><a href="/">Dropdown 1 </a></li>
+        <li><a href="/">Dropdown 2</a></li>
+        <li><a href="/">Dropdown 2</a></li>
+        <li><a href="/">Dropdown 3</a></li>
+        <li><a href="/">Dropdown 4</a></li>
+      </ul>
+    </li>
+    <li><a href="/">Pricing</a></li>
+    <li><a href="/">Contact</a></li>
+  </div>
+</ul>
+</nav>
+
       </header>
+      
     </div>
   );
 }
